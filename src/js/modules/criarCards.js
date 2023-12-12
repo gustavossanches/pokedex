ca76f688e1c8f9221ec26e5245c059c693bdfcf9
Matheus Brizola Jacob <matheusbj95@gmail.com>
2023-12-11 21:34:51 -0300

    WIP: module function createCards created

---
+++ src/js/modules/criarCards.js
@@ -0,0 +1,11 @@
+export default function createCards(pokemon) {
+  const section = document.createElement("section");
+  const img = document.createElement("img");
+  const div = document.createElement("div");
+  const h5 = document.createElement("h5");
+  const p = document.createElement("p");
+  const a = document.createElement("a");
+  
+}
+
+//TODO: nesse module, iremos criar a função que cria o card do exemplo no index.html usando o que aprendemos nas aulas de criação de elementos DOM (UC2 - aula 8 - dia 15/11). Depois de criar iremos exportar essa função para a script.js
