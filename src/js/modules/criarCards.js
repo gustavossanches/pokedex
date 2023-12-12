d8bb711fcdcfe53bbb955bcb38ab129949edb17b
Matheus Brizola <matheusbj95@gmail.com>
2023-12-12 20:53:35 -0300

    WIP: function criarCard terminado

--- src/js/modules/criarCards.js
+++ src/js/modules/criarCards.js
@@ -1,11 +1,28 @@
 export default function createCards(pokemon) {
-  const section = document.createElement("section");
+  // Creating HTML tags and variables for pokémon's data; 
+  const section = document.getElementById("container-card");
+  const p = document.createElement("p");
   const img = document.createElement("img");
   const div = document.createElement("div");
   const h5 = document.createElement("h5");
-  const p = document.createElement("p");
-  const a = document.createElement("a");
-  
+  let pokemonId = pokemon.id;
+  let pokemonName = pokemon.name;
+
+  // Inserting the HTML tags in their respective places
+  section.appendChild(p);
+  section.appendChild(img);
+  section.appendChild(div);
+  div.appendChild(h5);
+
+  // Adding classes to created tags
+  img.classList.add("card-img-top");
+  div.classList.add("card-body");
+  h5.classList.add("card-title");
+
+  // Inserting the pokémon's data in their respective HTML tags
+  p.appendChild(pokemonId);
+  h5.appendChild(pokemonName);
+
 }
 
 //TODO: nesse module, iremos criar a função que cria o card do exemplo no index.html usando o que aprendemos nas aulas de criação de elementos DOM (UC2 - aula 8 - dia 15/11). Depois de criar iremos exportar essa função para a script.js
