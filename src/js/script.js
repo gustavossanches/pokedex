e1313b6daae50281193941dbbbc3946fad4868d9
gustavossanches <gustavo.sanches80@hotmail.com>
2023-12-12 15:57:22 -0300

    Ajuste no js (fetch)

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
 console.log('1')
 
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=100')
-    .then(response => response.json())
-    .then(data => 
-        console.log(data.results))
-    .catch(error => console.log(error))
+
+const carregar = async () => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=100')
+        
+        const results = await response.json()
+        const all = results.results
+        for(let i of all) {
+            console.log(i)
+        }
+
+        console.log(all)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+carregar()
+
