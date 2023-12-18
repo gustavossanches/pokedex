c539e968df48fde4eec1d4a8dceb945de6aa4840
gustavossanches <gustavo.sanches80@hotmail.com>
2023-12-18 12:16:12 -0300

    WIP: Divisão nas funções do script

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,14 @@
 import createCards from './modules/criarCards.js'
 
+const arrayUrls = []
+const pokemonCardsArray = []
 
-const carregar = async () => {
+const getUrls = async () => {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=100')
-        const arrayUrls = []
-
         const results = await response.json()
         const all = results.results
-
-        //pegar url de todos os pokemons (um por um)
-        for (let pokemon of all) {
-            const idPokemon = pokemon.url.split('/')[6]
-            const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}/`
-
-            await fetch(urlPokemon)
-                .then(url => url.json())
-                .then(urlPokemon => arrayUrls.push(urlPokemon))//passando url de cada pokemon para o array criado aontes
-
-        }
-
-        //informações dos cards
-        const pokemonCardsArray = []
-        arrayUrls.forEach(pokemon => {
-            const nome = pokemon.forms[0].name
-            const imagem = pokemon.sprites.front_default
-            const id = pokemon.id
-            createCards(nome, imagem, id)
-            const pokemonCard = document.getElementById(`pokemon-${id}`)
-            pokemonCardsArray.push(pokemonCard)
-        });
-
-
-        //renderiza a pagina detail do pokemon especifico
-        // pokemonCardsArray.forEach((e) => {
-        //     e.addEventListener('click', (_) => {
-        //         const numero_id = e.id.split('-')[1]
-        //         window.location.href = `details.html?id=${numero_id}`;
-
-        //         const teste = document.getElementById('teste')
-        //         console.log(teste)
-        //         const testeTag = document.createElement("p");
-        //         teste.appendChild(testeTag)
-        //     })
-        // })
-
+        return all
 
 
     } catch (error) {
@@ -52,24 +16,47 @@ const carregar = async () => {
     }
 }
 
-carregar()
+const getUrlsPokemons = async () => {
+    //pegar url de todos os pokemons (um por um)
+    const all = await getUrls()
+    for (let pokemon of all) {
+        const idPokemon = pokemon.url.split('/')[6]
+        const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}/`
+
+        await fetch(urlPokemon)
+            .then(url => url.json())
+            .then(urlPokemon => arrayUrls.push(urlPokemon))//passando url de cada pokemon para o array criado aontes
+
+    }
+    return arrayUrls
+}
+
+
+const criarCards = async () => {
+    const urlsPokemons = await getUrlsPokemons()
+    urlsPokemons.forEach(pokemon => {
+        const nome = pokemon.forms[0].name
+        const imagem = pokemon.sprites.front_default
+        const id = pokemon.id
+        createCards(nome, imagem, id)
+    });
+}
+//informações dos cards
 
 
 
-/*
-   const teste = document.createElement('h1')
-            const testeTexto = document.createTextNode(i.name)
-            teste.appendChild(testeTexto)
-            const container = document.getElementById('container')
-            container.appendChild(teste)
-*/
+
+
+
+
+
+criarCards()
+
 
 const searchForm = document.querySelector("#search")
-
 searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const value = event.target.inputSearch.value;
-    console.log(value)
+    const value = event.target.inputSearch.value; //pega o valor digitado no input
     const teste = []
     const pokemonsFiltered = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=100000')
         .then(response => response.json())
@@ -81,13 +68,13 @@ searchForm.addEventListener("submit", async (event) => {
 
     const pokemonCardsArray = []
     filtros.forEach(pokemon => {
-            console.log(pokemon.name)
-            const nome = pokemon.forms[0].name
-            const imagem = pokemon.sprites.front_default
-            const id = pokemon.id
-            createCards(nome, imagem, id)
-            const pokemonCard = document.getElementById(`pokemon-${id}`)
-            pokemonCardsArray.push(pokemonCard)
-        });
+        console.log(pokemon.name)
+        const nome = pokemon.forms[0].name
+        const imagem = pokemon.sprites.front_default
+        const id = pokemon.id
+        createCards(nome, imagem, id)
+        const pokemonCard = document.getElementById(`pokemon-${id}`)
+        pokemonCardsArray.push(pokemonCard)
+    });
 
 })
