62c4cd52fe8655f31751cc931e36704fd088ffea
Gustavo Sanches <gustavo.sanches80@hotmail.com>
2023-12-19 21:37:20 -0300

    WIP: Adicionando botões previous e next

--- src/js/details.js
+++ src/js/details.js
@@ -1,38 +1,75 @@
 //pega o id da url do pokemon especifico(usar o fetch com esse id para ter elementos do pokemon especifico)
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
+let idd = id
 
-const carregaDetail = async () => {
+//teste
+/*
+function testandoAqui(n){
+    let soma = id
+    if(n == 0){
+        soma--
+    }else{
+        soma++
+    }
+    params.set('id', soma)
+    const updatedUrl = window.location.origin + window.location.pathname + "?" + params.toString();
+    window.location.href = updatedUrl;
+}
+*/
+
+//botao next
+const next = document.getElementById('next')
+next.addEventListener('click', () => {
+    let soma = id
+    soma++
+    params.set('id', soma)
+    const updatedUrl = window.location.origin + window.location.pathname + "?" + params.toString();
+    window.location.href = updatedUrl;
+})
+
+
+//botao previous
+const previous = document.getElementById('previous')
+if(id == 1){    //some o botao caso seja o primeiro pokemon
+    previous.style.display = 'none'
+}
+previous.addEventListener('click', () => {
+    let soma = id
+    soma--
+    params.set('id', soma)
+    const updatedUrl = window.location.origin + window.location.pathname + "?" + params.toString();
+    window.location.href = updatedUrl;
+})
+
+
+const carregaDetail = async (idd) => {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idd}`)
         const results = await response.json()
-        const nome = results.forms[0].name
 
+        //pega as informações do pokemon da url
+        const nome = results.forms[0].name
         const moves = results.moves
         const pokemonMoves = []
         moves.forEach(move => {
             pokemonMoves.push(move.move.name)
         });
-
         const pokemonTypes = []
         const types = results.types
         types.forEach(type => {
             pokemonTypes.push(type.type.name)
         })
-
         const height = results.height
         const weight = results.weight
-
         const image = results.sprites.front_default
         const imageShiny = results.sprites.front_shiny
-
         const pokemonEstatisticas = []
         const estatisticas = results.stats
         estatisticas.forEach(estatistica => {
             const baseStat = estatistica.base_stat
             pokemonEstatisticas.push(baseStat)
         })
-
         const hp = pokemonEstatisticas[0]
         const atk = pokemonEstatisticas[1]
         const def = pokemonEstatisticas[2]
@@ -41,10 +78,7 @@ const carregaDetail = async () => {
         const speed = pokemonEstatisticas[5]
 
 
-        //
-
-
-
+        //pega os ids e insere os dados
         const result_name = document.getElementById('result-name')
         result_name.innerHTML = nome
 
@@ -59,8 +93,6 @@ const carregaDetail = async () => {
 
         const result_weight = document.getElementById('result-weight')
         result_weight.innerHTML = weight
-
-
 
         const result_hps = document.getElementById('result-hp')
         result_hps.innerHTML = hp
@@ -79,7 +111,6 @@ const carregaDetail = async () => {
 
         const result_speed = document.getElementById('result-spd')
         result_speed.innerHTML = speed
-        
         
         const result_type = document.getElementById('result-type')
         result_type.innerHTML = pokemonTypes
@@ -100,4 +131,6 @@ const carregaDetail = async () => {
     }
 }
 
-carregaDetail()
+console.log(carregaDetail(idd))
+
+carregaDetail(idd)
